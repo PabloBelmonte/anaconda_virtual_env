ca8188faad4f4a7e0c8d4e5a76a8233d549909cb
unknown <pnabelmonte@gmail.com>
2021-01-22 00:21:22 -0300

    test
    test
    readme

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Files for the VLC virtual env setup.
 
 ## microsoft terminal
 Setup the microsoft terminal for the VLC anaconda virtual environment.
+
+	• Install Anaconda
+		○ https://www.anaconda.com/products/individual
+	• Open anaconda3 prompt (powershell)
+		○ Press the windows button and type "anaconda". Open it (powershell):
---
+++ vlc_virtual_env/install_other_packages.bat
@@ -0,0 +1,14 @@
+pip install bitstring
+pip install scikit-commpy
+pip install pyFFTW
+pip install reikna
+pip install tabulate
+pip install beeprint
+
+rem pip uninstall bitstring
+rem pip uninstall scikit-commpy
+rem pip uninstall pyFFTW
+rem pip uninstall reikna
+rem pip uninstall tabulate
+rem pip uninstall beeprint
+rem python.exe .\RUN.py
